68f63d09ae59a3ddb4081161c4e93425d2c89535
HassanZekrallah <hassan2206066@miuegypt.edu.eg>
2026-01-24 00:14:51 +0200

    hope last this time

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
-from fastapi import FastAPI, UploadFile
-from PIL import Image
 import torch
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image
 import torchvision.transforms as transforms
-import os
+import torchvision.models as models
+from model import DiabetesClassifier
+import io
 
 app = FastAPI()
 
-# Path to model
-MODEL_PATH = os.path.join("models", "D_model.pt")
+device = torch.device("cpu")
+MODEL_PATH = "models/D_model.pt"
 
-# Load model on CPU
-device = "cpu"
-model = torch.load(MODEL_PATH, map_location=device)
+# Load MobileNetV2 base model (NO pretrained)
+base_model = models.mobilenet_v2(pretrained=False)
+
+# Create classifier
+model = DiabetesClassifier(base_model)
+
+# Load weights
+model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
+model.to(device)
 model.eval()
 
-# Image preprocessing
+# Image preprocessing (MUST match training)
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                         std=[0.229, 0.224, 0.225])
 ])
 
 @app.get("/")
 def home():
-    return {"status": "API is running"}
+    return {"status": "Diabetes Detection API Running"}
 
-@app.post("/predict-image")
-async def predict_image(file: UploadFile):
-    img = Image.open(file.file).convert("RGB")
-    img = transform(img).unsqueeze(0)
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    image = transform(image).unsqueeze(0)
 
     with torch.no_grad():
-        output = model(img)
-        prediction = torch.argmax(output).item()
+        outputs = model(image)
+        _, predicted = torch.max(outputs, 1)
 
-    return {"prediction": int(prediction)}
+    class_names = ["diabetes" , "non_diabetes"]
+    return {"prediction": class_names[predicted.item()]}
